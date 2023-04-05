d9d6339414fcc71d016771ffaa64ce89e2850504
Oleksandr <lanko.oleksandr@gmail.com>
2023-04-05 23:04:51 +0300

    youtube to full scren

--- src/js/search/createModalWindow.js
+++ src/js/search/createModalWindow.js
@@ -58,6 +58,7 @@ class Video {
     constructor(obj) {
         this.video = document.createElement('iframe');
         this.video.src = `https://www.youtube.com/embed/${getTrailer(obj.videos.results)}`;
+        this.video.setAttribute("allowfullscreen", "true");
         return this.video;
     }
 }

8d3deb1439f20519278d80b5cebe359d63f5a693
tromp86 <youWin@gmail.com>
2023-04-05 11:53:40 +0300

    start 05/04/2023

--- src/start/start.js
+++ src/start/start.js
@@ -1,26 +1,48 @@
+"use strict";
+
+// MODAL
+const singInBtn = document.getElementById('singIn');
+const singUpBtn = document.getElementById('singUp');
+const firstForm = document.getElementById('form1');
+const secondForm = document.getElementById('form2');
+const container = document.querySelector('.container');
+
+singInBtn.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+singUpBtn.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+firstForm.addEventListener('submit', (e) => e.preventDefault());
+secondForm.addEventListener('submit', (e) => e.preventDefault());
+
+// START
 const loginForm = document.getElementById("login-form");
+const myModal = document.getElementById("myModal");
+const modalMain = document.querySelector("main");
+const body = document.body;
+
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  const myModal = document.getElementById("myModal");
-  const modalMain = document.querySelector("main");
   myModal.classList.remove("hidden");
   myModal.classList.add("myModalMain");
 
-  document.body.classList.add("blur");
+  body.classList.add("blur");
   loginForm.style.visibility = "hidden";
+});
 
-  document.body.addEventListener("click", function (event) {
-    if (event.target == modalMain) {
-      myModal.style.display = "none";
-      modalMain.style.visibility = "visible";
-      document.body.classList.remove("blur");
-      modalMain.style.filter = "none";
-      loginForm.style.visibility = "visible";
-    }
-  });
-  loginForm.addEventListener("click", function (event) {
-    myModal.style.display = "block";
-  });
+body.addEventListener("click", function (event) {
+  if (event.target == modalMain) {
+    myModal.style.display = "none";
+    modalMain.style.visibility = "visible";
+    body.classList.remove("blur");
+    modalMain.style.filter = "none";
+    loginForm.style.visibility = "visible";
+  }
+});
+
+loginForm.addEventListener("click", function (event) {
+  myModal.style.display = "block";
 });
 
 function registerUser() {
@@ -28,38 +50,46 @@ function registerUser() {
   let email = document.querySelector('input[type="email"]').value;
   let password = document.querySelector('input[type="password"]').value;
 
+  if (localStorage.getItem(email)) {
+    console.log("User already exists. Please choose a different email address.");
+    return;
+  }
+
   let user = {
     username: username,
     email: email,
     password: password,
   };
-
   localStorage.setItem(email, JSON.stringify(user));
 
   document.querySelector('input[type="text"]').value = "";
   document.querySelector('input[type="email"]').value = "";
   document.querySelector('input[type="password"]').value = "";
   console.log("Registration successful.");
+  container.classList.remove('right-panel-active');
 }
 
 function loginUser() {
   let email = document.getElementById("inputLog");
   let password = document.getElementById("inputPass");
   let userData = JSON.parse(localStorage.getItem(email.value), "userData");
-  
+
   if (userData.email === email.value && userData.password === password.value) {
-    console.log('Login successful. Welcome ' + userData.username + '!');
-  startUserIcon();
-  if (userData) {
-    myModal.style.display = "none";
-    const startBtn = document.getElementById("startBtn");
-    startBtn.style.display = "block";
-    let ready = document.querySelector(".ready");
-    ready.textContent = "";
-  }
+    console.log("Login successful. Welcome " + userData.username + "!");
+    startUserIcon();
+    window.onload = function() {
+      loginUser();
+    }
+    if (userData) {
+      myModal.style.display = "none";
+      const startBtn = document.getElementById("startBtn");
+      startBtn.style.display = "block";
+      let ready = document.querySelector(".ready");
+      ready.textContent = "";
+    }
   } else {
     console.log("Incorrect email or password. Please try again.");
-  } 
+  }
   document.querySelector('input[type="email"]').value = "";
   document.querySelector('input[type="password"]').value = "";
 }
@@ -68,48 +98,74 @@ function startUserIcon() {
   let email = document.getElementById("inputLog");
   let userData = JSON.parse(localStorage.getItem(email.value), "userData");
 
-  const userIcon = document.createElement("img");
-  userIcon.src = "./img/user-icon.png";
-  userIcon.alt = "User Icon";
-  userIcon.width = 33;
-  userIcon.height = 33;
-  userIcon.classList.add("userIcon");
+  if (userData) {
+    const userIcon = document.createElement("img");
+    userIcon.src = "./img/user-icon.png";
+    userIcon.alt = "User Icon";
+    userIcon.width = 33;
+    userIcon.height = 33;
+    userIcon.classList.add("userIcon");
 
-  const usernameIcon = document.createElement("span");
-  usernameIcon.textContent = userData.username + " " + userData.email;
-  const dropdown = document.createElement("div");
-  dropdown.classList.add("dropdown");
-  dropdown.appendChild(usernameIcon);
+    const usernameIcon = document.createElement("span");
+    usernameIcon.textContent = userData.username;
+    const dropdown = document.createElement("div");
+    dropdown.classList.add("dropdown");
+    dropdown.appendChild(usernameIcon);
 
-  const dropdownContent = document.createElement("div");
-  dropdownContent.classList.add("dropdown-content");
-  const div1 = document.createElement("div");
-  div1.classList.add("dropdown-content");
-  const profileLink = document.createElement("a");
-  profileLink.href = "#";
-  profileLink.textContent = "Settings";
-  dropdownContent.appendChild(profileLink);
-  dropdownContent.appendChild(div1);
-  const div2 = document.createElement("div");
-  const logoutLink = document.createElement("a");
-  logoutLink.href = "#";
-  logoutLink.textContent = "Log Out";
-  dropdownContent.appendChild(div2);
-  dropdownContent.appendChild(logoutLink);
-  dropdown.appendChild(dropdownContent);
-  logoutLink.addEventListener("click", function (event) {
-    event.preventDefault();
-    localStorage.removeItem(userData.email);
-    location.reload();
-  });
+    const dropdownContent = document.createElement("div");
+    dropdownContent.classList.add("dropdown-content");
+    const div1 = document.createElement("div");
+    div1.classList.add("dropdown-content");
+    const profileLink = document.createElement("a");
+    profileLink.href = "#";
+    profileLink.textContent = "Settings";
+    dropdownContent.appendChild(profileLink);
+    dropdownContent.appendChild(div1);
+    const div2 = document.createElement("div");
+    const logoutLink = document.createElement("a");
+    logoutLink.href = "#";
+    logoutLink.textContent = "Log Out";
+    dropdownContent.appendChild(div2);
+    dropdownContent.appendChild(logoutLink);
+    dropdown.appendChild(dropdownContent);
+    logoutLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      location.reload();
+    });
 
-  const userDiv = document.querySelector(".user");
-  userDiv.appendChild(userIcon);
-  userDiv.appendChild(dropdown);
+    const userDiv = document.querySelector(".user");
+    userDiv.appendChild(userIcon);
+    userDiv.appendChild(dropdown);
+  }
 }
 
+// ПЕРЕВІРКА ПОШТИ
+const emailInput = document.getElementById('inputLogMain');
+const emailStatus = document.getElementById('email-status');
+
+emailInput.addEventListener('input', function() {
+  const email = emailInput.value;
+  const user = JSON.parse(localStorage.getItem(email));
+  if (user) {
+    emailStatus.classList.remove('invalid');
+    emailStatus.classList.add('valid');
+    emailStatus.textContent = 'Email found!';
+    console.log("Email exists in localStorage.");
+    setTimeout(() => {
+      location.href = "../main/main.html";
+    }, 500);
+    
+  } else {
+    emailStatus.classList.remove('valid');
+    emailStatus.classList.add('invalid');
+    emailStatus.textContent = 'Email not found.';
+    console.log("Email does not exist in localStorage.");
+  }
+});
 
 
 // export default username;
 // export default userData;
 // import { startUserIcon } from "./startUser.js";
+
+

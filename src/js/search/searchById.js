9082c5f6c21c8322e3c68a1d33a83e04f8575b08
titaniym21 <72406204+titaniym21@users.noreply.github.com>
2023-04-06 16:25:05 +0300

    fix

--- src/js/search/searchById.js
+++ src/js/search/searchById.js
@@ -4,8 +4,8 @@ let windowModal;
 const { CreateModalWindow } = require('./createModalWindow');
 
 function searchById(id) {
-        let search2 = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=videos`;
-        let search = `https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&append_to_response=videos`;
+        let search = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=videos`;
+        //let search2 = `https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&append_to_response=videos`;
         fetch(search)
             .then((data) => data.json())
             .then((obj) => {
@@ -21,27 +21,27 @@ function searchById(id) {
                     }
                 } catch (error) {
                     console.log(error);
-                    fetch(search2)
-                        .then((data) => data.json())
-                        .then((obj) => {
-                            try {
-                                if (obj.success === false) {
-                                    throw new Error('The resource you requested could not be found.');
-                                } else {
-                                    let bg = document.createElement('section');
-                                    bg.classList.add('bg');
-                                    body.append(bg);
-                                    windowModal = new CreateModalWindow(obj);
-                                    body.append(windowModal);
-                                }
-                            } catch (error) {
-                                console.log(error);
-                            }
-                        })
-                        .catch((error) => console.log(error));
+                    //fetch(search2)
+                        //.then((data) => data.json())
+                        // .then((obj) => {
+                        //     try {
+                        //         if (obj.success === false) {
+                        //             throw new Error('The resource you requested could not be found.');
+                        //         } else {
+                        //             let bg = document.createElement('section');
+                        //             bg.classList.add('bg');
+                        //             body.append(bg);
+                        //             windowModal = new CreateModalWindow(obj);
+                        //             body.append(windowModal);
+                        //         }
+                        //     } catch (error) {
+                        //         console.log(error);
+                        //     }
+                        // })
+                        //.catch((error) => console.log(error));
                 }
             })
-            .catch((error) => console.log(error));
+        .catch((error) => console.log(error));
     }
 
 module.exports = searchById;
